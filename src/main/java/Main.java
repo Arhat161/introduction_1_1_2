99e2acf02c471ab29d147605d5dc2dbf57c1b629
Arhat161 <Arhat161@users.noreply.github.com>
2022-07-01 17:02:29 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int value1 = 512; //переменная 1
+        int value2 = 256; //переменная 2
+        System.out.println(value1 - value2);
+    }
+}
